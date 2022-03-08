9a98e4f0eb50489d45770085911a34bc25cb9c1c
Paul Matar <paul_matar@icloud.com>
2022-03-08 22:43:37 +0200

    gradient layer added

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -15,9 +15,21 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setGradientBackground(colorTop: .systemPink, colorBottom: .systemIndigo)
+        
         welcomeLabel.text = "Welcome, \(userName ?? "")!"
-
     }
-
     
+    private func setGradientBackground(colorTop: UIColor, colorBottom: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        
+        gradientLayer.colors = [colorBottom.cgColor, colorTop.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.locations = [0, 1]
+        gradientLayer.frame = view.bounds
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
