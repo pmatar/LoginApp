a166dd37a9d4599b7dd6074c7a17f1687258c6b5
Paul Matar <paul_matar@icloud.com>
2022-03-08 16:39:08 +0200

    constraints and storyboard setup

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Paul Matar on 08.03.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var usernameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func login(_ sender: UIButton) {
+    }
+    
+}
+
